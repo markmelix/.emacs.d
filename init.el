0afdcf88a2ed7761b7d01acbcaf058f42e726a55
Mark Meliksetyan <markmelix@gmail.com>
2021-11-03 14:45:36 +0300

    Refactor entire config.org and take straight bootstrapper out of it
    Changes:
    - Get rid of the evil-mode because setting its keybindings for all
      packages up is a headache. I'm tired of it. Now I use default
      Emacs keybindings.
    - Append ~/.local/bin to the Emacs exec-path.
    - Set gc-cons-threshold to 100000000 only for startup time.
    - Add blank lines before each heading
    - Change open-init-file function to open config.org instead of init.el
    - Fix indentation
    - Change description for some setting items
    - Display line numbers in every buffer
    - Change scrolling settings
    - Fix bug with final new line
    - Set tab width to 4
    - Disable inhibiting of electric indent
    - Enable closing of parenthesis automatically
    - Enable auto-fill mode for some major modes
    - Setup input method to the Russian settings
    - Change X clipboard behavior
    - Take straight bootstrapper out of config.org to the init.el file
    - Take org-mode installation out of config.org cause of a bug with
      org version difference
    - Add which-key, general, flycheck, company-box, dap-mode, python-mode,
      pyenv, cmake-mode, magit, org-fragtog packages
    - Remove undo-tree, keychord and all evil packages
    - Move custom file in no-littering package
    - Change settings for lsp-mode, lsp-ui and company packages
    - Setup org-mode and org-roam packages

--- init.el
+++ init.el
@@ -1,4 +1,21 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+			 user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 (concat "https://raw.githubusercontent.com/raxod502/straight.el"
+		 "/develop/install.el")
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
 (require 'org)
+
 (org-babel-load-file
  (expand-file-name "config.org"
 		   user-emacs-directory))
