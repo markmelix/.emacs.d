d2591839095fb31e98fadd6c306513ad1d186cc2
Mark Meliksetyan <markmelix@gmail.com>
2022-04-11 14:11:44 +0300

    eglot: move from setq to custom-set-variables

--- elisp/ide/eglot.el
+++ elisp/ide/eglot.el
@@ -4,10 +4,10 @@
 
 (require 'eglot)
 
-(setq rustic-lsp-client 'eglot)
+(custom-set-variables '(rustic-lsp-client 'eglot))
 
 (keymap-set eglot-mode-map "C-c r" 'eglot-rename)
 (keymap-set eglot-mode-map "C-c h" 'eldoc)
 (keymap-set eglot-mode-map "C-c C-a" 'eglot-code-actions)
 
-;(setq eglot-ignored-server-capabilities '(:documentHighlightProvider))
+;(custom-set-variables '(eglot-ignored-server-capabilities '(:documentHighlightProvider)))
