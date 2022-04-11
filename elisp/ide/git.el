ad9da024a9ae9373bd651286d2dbaf01bff1458c
Mark Meliksetyan <markmelix@gmail.com>
2022-04-11 14:14:32 +0300

    Enhance git management

--- elisp/ide/git.el
+++ elisp/ide/git.el
@@ -2,3 +2,29 @@
 
 (straight-use-package 'magit)
 (require 'magit)
+
+(straight-use-package 'magit-todos)
+(require 'magit-todos)
+(add-hook 'magit-mode-hook 'magit-todos-mode)
+
+;;; Step through historic versions of git controlled file
+(straight-use-package 'git-timemachine)
+(keymap-global-set "C-c g t" 'git-timemachine)
+
+;;; Popup commit message at current line to know why this line was changed
+(straight-use-package 'git-messenger)
+(keymap-global-set "C-c g m" 'git-messenger:popup-message)
+(custom-set-variables '(git-messenger:show-detail t)
+					  '(git-messenger:use-magit-popup t))
+
+;;; Open page at github/bitbucket from emacs buffers
+(straight-use-package 'browse-at-remote)
+(keymap-global-set "C-c g b" 'browse-at-remote)
+
+;;; Highlight diffs in buffer
+(straight-use-package 'diff-hl)
+(global-diff-hl-mode)
+
+;; Magit integration
+(add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
