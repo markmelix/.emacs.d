a4d5b1caf0045127e3090081063244501d4ba652
Mark Meliksetyan <markmelix@gmail.com>
2022-04-11 14:14:56 +0300

    Fix some errors related to projectile

--- elisp/ide/projectile.el
+++ elisp/ide/projectile.el
@@ -1,4 +1,15 @@
 ;;;; Project management
 (straight-use-package 'projectile)
-(keymap-global-set "C-c p" projectile-command-map)
+(keymap-global-set "C-c p" 'projectile-command-map)
 (projectile-mode 1)
+
+;;; Set default shell to bash instead of fish before switchong project.
+;;; 
+;;; Otherwise projectile raises error when it tries to open a project. See
+;;; https://github.com/hlissner/doom-emacs/issues/1569
+(add-hook 'projectile-before-switch-project-hook
+		  '(lambda () (setq shell-file-name "/bin/bash")))
+
+;; Projectile with helm
+(straight-use-package 'helm-projectile)
+(helm-projectile-on)
