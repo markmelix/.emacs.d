25b9b36afa8161d5630d09da73f616354f7ea91d
Mark Meliksetyan <markmelix@gmail.com>
2022-04-11 14:12:04 +0300

    eldoc: move from setq to custom-set-variables

--- elisp/ide/eldoc.el
+++ elisp/ide/eldoc.el
@@ -1,6 +1,7 @@
 ;;;; Eldoc configuration
 
 (global-eldoc-mode 1)
-(setq ;eldoc-echo-area-use-multiline-p 5
-	  eldoc-idle-delay 1
-	  eldoc-echo-area-display-truncation-message nil)
+(custom-set-variables
+ ;'(eldoc-echo-area-use-multiline-p 5)
+ '(eldoc-idle-delay 1)
+ '(eldoc-echo-area-display-truncation-message nil))
